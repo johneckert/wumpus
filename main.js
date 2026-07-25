7bc27f94096ae76c63b8323ee6474aa4f598e31b
John Eckert <johnteckert@gmail.com>
2026-07-25 17:39:15 -0400

    layout event listeners for basic controls

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-/*
-    * radomly generate traps (probably part of setup to gen x number and then place them)
-    * randomly generate wumpus location (does it move?)
-    * randomly generate start position (but not near wumpus)
-*/
-
 const width = 10;
 const height = width;
 const seed = undefined;
 const numTraps = 4;
 const trapLocations = [];
-let wumpus = false;
-let player = false
+let wumpus = false; // { x, y, index }
+let player = false; // { x, y, index }
+let arrowMode = false;
+
+function movePlayer(key) {
+    switch (key) {
+        case "ArrowUp":
+            console.log('U');
+            break;
+        case "ArrowRight":
+            console.log('R');
+            break;
+        case "ArrowDown":
+            console.log('D');
+            break;
+        case "ArrowLeft":
+            console.log('L');
+            break;
+    }
+};
+
+function fireArrow(key) {
+    switch (key) {
+        case "ArrowUp":
+            console.log('Arrow U');
+            break;
+        case "ArrowRight":
+            console.log('Arrow R');
+            break;
+        case "ArrowDown":
+            console.log('Arrow D');
+            break;
+        case "ArrowLeft":
+            console.log('Arrow L');
+            break;
+    }
+};
 
 const cells = generateMaze(width, height, seed);
 
@@ -42,46 +71,73 @@ while (!wumpus) {
 }
 
 // place player
+while (!player) {
+    let pI = Math.floor(Math.random() * cells.length);
+    if (!cells[pI].isPit && !cells[pI].isWarning && !cells[pI].isWumpus) {
+        cells[pI].isPlayer = true;
+        player = { x: cells[pI].posX, y: cells[pI].posY, i: pI };
+    }
+}
+
 
 // draw gameboard
-const gameBoard = document.getElementById("game-board");
-cells.forEach(cell => {
-    const cellDiv = document.createElement('div')
-    cellDiv.classList.add('cell');
-    if (cell.exitTop) {
-        cellDiv.classList.add('top-door');
-    }
-    if (cell.exitRight) {
-        cellDiv.classList.add('right-door');
-    }
-    if (cell.exitDown) {
-        cellDiv.classList.add('bottom-door');
-    }
-    if (cell.exitLeft) {
-        cellDiv.classList.add('left-door');
-    }
-    if (cell.isPit) {
-        cellDiv.classList.add('trap');
-    }
-    if (cell.isWumpus) {
-        cellDiv.classList.add('wumpus');
-    }
-    if (cell.isPlayer) {
-        cellDiv.classList.add('isPlayer');
-    }
-    if (cell.isWarning) {
-        cellDiv.classList.add('warning');
-    }
+function drawGameboard () {
+    const gameBoard = document.getElementById("game-board");
+    gameBoard.innerHTML = "";
+    cells.forEach(cell => {
+        const cellDiv = document.createElement('div')
+        cellDiv.classList.add('cell');
+        if (arrowMode) {
+            cellDiv.classList.add('arrow-mode');
+        }
+        if (cell.exitTop) {
+            cellDiv.classList.add('top-door');
+        }
+        if (cell.exitRight) {
+            cellDiv.classList.add('right-door');
+        }
+        if (cell.exitDown) {
+            cellDiv.classList.add('bottom-door');
+        }
+        if (cell.exitLeft) {
+            cellDiv.classList.add('left-door');
+        }
+        if (cell.isPit) {
+            cellDiv.classList.add('trap');
+        }
+        if (cell.isWumpus) {
+            cellDiv.classList.add('wumpus');
+        }
+        if (cell.isPlayer) {
+            cellDiv.classList.add('isPlayer');
+        }
+        if (cell.isWarning) {
+            cellDiv.classList.add('warning');
+        }
+        if (cell.isPlayer) {
+            cellDiv.classList.add('player');
+        }
 
+        cellDiv.style = `grid-column-start: ${cell.posX}; grid-column-end: ${cell.posX + 1}; grid-row-start: ${cell.posY}; grid-row-end: ${cell.posY + 1};`
+        cellDiv.innerText = `${cell.posX}, ${cell.posY}`;
+        gameBoard.appendChild(cellDiv)
+    })
+};
 
+drawGameboard();
 
-
-
-    cellDiv.style = `grid-column-start: ${cell.posX}; grid-column-end: ${cell.posX + 1}; grid-row-start: ${cell.posY}; grid-row-end: ${cell.posY + 1};`
-    cellDiv.innerText = `${cell.posX}, ${cell.posY}`;
-    gameBoard.appendChild(cellDiv)
+// event listeners
+document.addEventListener("keyup", ({ key }) => {
+    if (key === 'a') {
+        arrowMode = !arrowMode;
+        drawGameboard();
+    }
+    if (key === "ArrowUp" || key === "ArrowRight" || key === "ArrowDown" || key === "ArrowLeft") {
+        if (arrowMode) {
+            fireArrow(key)
+        } else {
+            movePlayer(key)
+        }
+    }
 })
-
-
-console.log(cells);
 
