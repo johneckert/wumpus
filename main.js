9d69cd043ba1798bf2ad75fbe16ce32d5e81708c
John Eckert <johnteckert@gmail.com>
2026-07-26 12:16:28 -0400

    basic movement - doors are buggy

--- main.js
+++ main.js
@@ -2,24 +2,100 @@ const width = 10;
 const height = width;
 const seed = undefined;
 const numTraps = 4;
-const trapLocations = [];
-let wumpus = false; // { x, y, index }
-let player = false; // { x, y, index }
 let arrowMode = false;
+const { cells, trapsLocations, player, wumpus } = createGame();
+
+function createGame() {
+    const cells = generateMaze(width, height, seed);
+    const trapLocations = [];
+    let player, wumpus;
+    // add traps
+    let placedTraps = 0;
+    while (placedTraps < numTraps) {
+        let tI = Math.floor(Math.random() * cells.length);
+        if (!cells[tI].isPit) {
+            cells[tI].isPit = true;
+            trapLocations.push(tI);
+            placedTraps++;
+        }
+    }
+
+    // place Wumpus
+    while (!wumpus) {
+        let wI = Math.floor(Math.random() * cells.length);
+        if (!cells[wI].isPit) {
+            cells[wI].isWumpus = true;
+            const x = cells[wI].posX;
+            const y = cells[wI].posY;
+            cells.map(cell => {
+                if (cell.isNeghbor(x, y)) {
+                    cell.isWarning = true;
+                }
+            });
+            wumpus = { x, y, i: wI };
+        }
+    }
+
+    // place player
+    while (!player) {
+        let pI = Math.floor(Math.random() * cells.length);
+        if (!cells[pI].isPit && !cells[pI].isWarning && !cells[pI].isWumpus) {
+            cells[pI].isPlayer = true;
+            player = { x: cells[pI].posX, y: cells[pI].posY, i: pI };
+        }
+    }
+    return { cells, trapLocations, player, wumpus };
+}
+
+function updatePlayerLocation(newCell, oldCell) {
+    newCell.isPlayer = true;
+    oldCell.isPlayer = false;
+    player.x = newCell.posX;
+    player.y = newCell.posY;
+    player.i = cells.indexOf(newCell);
+    drawGameboard();
+}
 
 function movePlayer(key) {
+    const currentCell = cells[player.i]
+    console.log(player);
+    console.log(currentCell);
     switch (key) {
         case "ArrowUp":
             console.log('U');
+            if (currentCell.exitTop) {
+                const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y - 1)[0];
+                updatePlayerLocation(newCell, currentCell);
+            } else {
+                console.log("Bonk!");
+            }
             break;
         case "ArrowRight":
             console.log('R');
+            if (currentCell.exitRight) {
+                const newCell = cells.filter((cell) => cell.posX === player.x + 1 && cell.posY === player.y)[0];
+                updatePlayerLocation(newCell, currentCell);
+            } else {
+                console.log("Bonk!");
+            }
             break;
         case "ArrowDown":
             console.log('D');
+            if (currentCell.exitBottom) {
+                const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y + 1)[0];
+                updatePlayerLocation(newCell, currentCell);
+            } else {
+                console.log("Bonk!");
+            }
             break;
         case "ArrowLeft":
             console.log('L');
+            if (currentCell.exitLeft) {
+                const newCell = cells.filter((cell) => cell.posX === player.x - 1 && cell.posY === player.y)[0];
+                updatePlayerLocation(newCell, currentCell);
+            } else {
+                console.log("Bonk!");
+            }
             break;
     }
 };
@@ -41,43 +117,6 @@ function fireArrow(key) {
     }
 };
 
-const cells = generateMaze(width, height, seed);
-
-// add traps
-let placedTraps = 0;
-while (placedTraps < numTraps) {
-    let tI = Math.floor(Math.random() * cells.length);
-    if (!cells[tI].isPit) {
-        cells[tI].isPit = true;
-        trapLocations.push(tI);
-        placedTraps++;
-    }
-}
-
-// place Wumpus
-while (!wumpus) {
-    let wI = Math.floor(Math.random() * cells.length);
-    if (!cells[wI].isPit) {
-        cells[wI].isWumpus = true;
-        const x = cells[wI].posX;
-        const y = cells[wI].posY;
-        cells.map(cell => {
-            if (cell.isNeghbor(x, y)) {
-                cell.isWarning = true;
-            }
-        });
-        wumpus = { x, y, i: wI };
-    }
-}
-
-// place player
-while (!player) {
-    let pI = Math.floor(Math.random() * cells.length);
-    if (!cells[pI].isPit && !cells[pI].isWarning && !cells[pI].isWumpus) {
-        cells[pI].isPlayer = true;
-        player = { x: cells[pI].posX, y: cells[pI].posY, i: pI };
-    }
-}
 
 
 // draw gameboard
