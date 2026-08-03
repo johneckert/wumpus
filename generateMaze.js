4015880842fb22f6f8f9e66780a12f5f776e59f6
John Eckert <johnteckert@gmail.com>
2026-08-03 13:44:39 -0400

    change tab 4 > tab2

--- generateMaze.js
+++ generateMaze.js
@@ -5,27 +5,27 @@
 // --------------------------------------------------------------------
 
 class Cell {
-    constructor(posX, posY) {
-        this.posX = posX;
-        this.posY = posY;
-        this.exitTop = false;
-        this.exitLeft = false;
-        this.exitRight = false;
-        this.exitDown = false;
-        this.isPit = false;
-        this.isWarning = false;
-        this.isWumpus = false;
-        this.isPlayer = false;
-        this.visited = false;
-    }
+  constructor(posX, posY) {
+    this.posX = posX;
+    this.posY = posY;
+    this.exitTop = false;
+    this.exitLeft = false;
+    this.exitRight = false;
+    this.exitDown = false;
+    this.isPit = false;
+    this.isWarning = false;
+    this.isWumpus = false;
+    this.isPlayer = false;
+    this.visited = false;
+  }
 
-    isNeghbor(x, y) {
-        return (this.posX >= x - 1 && this.posX <= x + 1) && (this.posY >= y - 1 && this.posY <= y + 1) ? true : false;
-    }
+  isNeghbor(x, y) {
+    return (this.posX >= x - 1 && this.posX <= x + 1) && (this.posY >= y - 1 && this.posY <= y + 1) ? true : false;
+  }
 
-    visit() {
-        this.visited = true;
-    }
+  visit() {
+    this.visited = true;
+  }
 }
 
 // --------------------------------------------------------------------
@@ -33,14 +33,14 @@ class Cell {
 // --------------------------------------------------------------------
 
 function mulberry32(seed) {
-    let a = seed >>> 0;
-    return function () {
-        a |= 0;
-        a = (a + 0x6d2b79f5) | 0;
-        let t = Math.imul(a ^ (a >>> 15), 1 | a);
-        t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
-        return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
-    };
+  let a = seed >>> 0;
+  return function () {
+    a |= 0;
+    a = (a + 0x6d2b79f5) | 0;
+    let t = Math.imul(a ^ (a >>> 15), 1 | a);
+    t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
 }
 
 // --------------------------------------------------------------------
@@ -53,34 +53,34 @@ const FRONTIER = 0x20;
 const OPPOSITE = { [E]: W, [W]: E, [N]: S, [S]: N };
 
 function addFrontier(x, y, grid, frontier) {
-    if (x >= 0 && y >= 0 && y < grid.length && x < grid[y].length && grid[y][x] === 0) {
-        grid[y][x] |= FRONTIER;
-        frontier.push([x, y]);
-    }
+  if (x >= 0 && y >= 0 && y < grid.length && x < grid[y].length && grid[y][x] === 0) {
+    grid[y][x] |= FRONTIER;
+    frontier.push([x, y]);
+  }
 }
 
 function mark(x, y, grid, frontier) {
-    grid[y][x] |= IN;
-    addFrontier(x - 1, y, grid, frontier);
-    addFrontier(x + 1, y, grid, frontier);
-    addFrontier(x, y - 1, grid, frontier);
-    addFrontier(x, y + 1, grid, frontier);
+  grid[y][x] |= IN;
+  addFrontier(x - 1, y, grid, frontier);
+  addFrontier(x + 1, y, grid, frontier);
+  addFrontier(x, y - 1, grid, frontier);
+  addFrontier(x, y + 1, grid, frontier);
 }
 
 function neighbors(x, y, grid) {
-    const n = [];
-    if (x > 0 && (grid[y][x - 1] & IN) !== 0) n.push([x - 1, y]);
-    if (x + 1 < grid[y].length && (grid[y][x + 1] & IN) !== 0) n.push([x + 1, y]);
-    if (y > 0 && (grid[y - 1][x] & IN) !== 0) n.push([x, y - 1]);
-    if (y + 1 < grid.length && (grid[y + 1][x] & IN) !== 0) n.push([x, y + 1]);
-    return n;
+  const n = [];
+  if (x > 0 && (grid[y][x - 1] & IN) !== 0) n.push([x - 1, y]);
+  if (x + 1 < grid[y].length && (grid[y][x + 1] & IN) !== 0) n.push([x + 1, y]);
+  if (y > 0 && (grid[y - 1][x] & IN) !== 0) n.push([x, y - 1]);
+  if (y + 1 < grid.length && (grid[y + 1][x] & IN) !== 0) n.push([x, y + 1]);
+  return n;
 }
 
 function direction(fx, fy, tx, ty) {
-    if (fx < tx) return E;
-    if (fx > tx) return W;
-    if (fy < ty) return S;
-    if (fy > ty) return N;
+  if (fx < tx) return E;
+  if (fx > tx) return W;
+  if (fy < ty) return S;
+  if (fy > ty) return N;
 }
 
 // --------------------------------------------------------------------
@@ -92,42 +92,42 @@ function direction(fx, fy, tx, ty) {
 // --------------------------------------------------------------------
 
 function generateMaze(width, height, seed = Math.floor(Math.random() * 0xffffffff)) {
-    const rng = mulberry32(seed);
-    const rand = (n) => Math.floor(rng() * n);
+  const rng = mulberry32(seed);
+  const rand = (n) => Math.floor(rng() * n);
 
-    const grid = Array.from({ length: height }, () => Array(width).fill(0));
-    const frontier = [];
+  const grid = Array.from({ length: height }, () => Array(width).fill(0));
+  const frontier = [];
 
-    mark(rand(width), rand(height), grid, frontier);
+  mark(rand(width), rand(height), grid, frontier);
 
-    while (frontier.length > 0) {
-        const idx = rand(frontier.length);
-        const [x, y] = frontier.splice(idx, 1)[0];
-        const n = neighbors(x, y, grid);
-        const [nx, ny] = n[rand(n.length)];
+  while (frontier.length > 0) {
+    const idx = rand(frontier.length);
+    const [x, y] = frontier.splice(idx, 1)[0];
+    const n = neighbors(x, y, grid);
+    const [nx, ny] = n[rand(n.length)];
 
-        const dir = direction(x, y, nx, ny);
-        grid[y][x] |= dir;
-        grid[ny][nx] |= OPPOSITE[dir];
+    const dir = direction(x, y, nx, ny);
+    grid[y][x] |= dir;
+    grid[ny][nx] |= OPPOSITE[dir];
 
-        mark(x, y, grid, frontier);
+    mark(x, y, grid, frontier);
+  }
+
+  // Convert the bitmask grid into an array of Cell instances
+  const cells = [];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const cell = new Cell(x, y);
+      const bits = grid[y][x];
+      cell.posX = x;
+      cell.posY = y;
+      cell.exitTop = (bits & N) !== 0;
+      cell.exitDown = (bits & S) !== 0;
+      cell.exitRight = (bits & E) !== 0;
+      cell.exitLeft = (bits & W) !== 0;
+      cells.push(cell);
     }
+  }
 
-    // Convert the bitmask grid into an array of Cell instances
-    const cells = [];
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            const cell = new Cell(x, y);
-            const bits = grid[y][x];
-            cell.posX = x;
-            cell.posY = y;
-            cell.exitTop = (bits & N) !== 0;
-            cell.exitDown = (bits & S) !== 0;
-            cell.exitRight = (bits & E) !== 0;
-            cell.exitLeft = (bits & W) !== 0;
-            cells.push(cell);
-        }
-    }
-
-    return cells;
+  return cells;
 }
--- main.js
+++ main.js
@@ -8,239 +8,239 @@ let arrowMode = false;
 const { cells, trapsLocations, player, wumpus } = createGame();
 
 function createGame() {
-    const cells = generateMaze(width, height, seed);
-    const trapLocations = [];
-    let player, wumpus;
-    // add traps
-    let placedTraps = 0;
-    while (placedTraps < numTraps) {
-        let tI = Math.floor(Math.random() * cells.length);
-        if (!cells[tI].isPit) {
-            cells[tI].isPit = true;
-            trapLocations.push(tI);
-            placedTraps++;
-        }
+  const cells = generateMaze(width, height, seed);
+  const trapLocations = [];
+  let player, wumpus;
+  // add traps
+  let placedTraps = 0;
+  while (placedTraps < numTraps) {
+    let tI = Math.floor(Math.random() * cells.length);
+    if (!cells[tI].isPit) {
+      cells[tI].isPit = true;
+      trapLocations.push(tI);
+      placedTraps++;
     }
+  }
 
-    // place Wumpus
-    while (!wumpus) {
-        let wI = Math.floor(Math.random() * cells.length);
-        if (!cells[wI].isPit) {
-            cells[wI].isWumpus = true;
-            const x = cells[wI].posX;
-            const y = cells[wI].posY;
-            cells.map(cell => {
-                if (cell.isNeghbor(x, y)) {
-                    cell.isWarning = true;
-                }
-            });
-            wumpus = { x, y, i: wI };
+  // place Wumpus
+  while (!wumpus) {
+    let wI = Math.floor(Math.random() * cells.length);
+    if (!cells[wI].isPit) {
+      cells[wI].isWumpus = true;
+      const x = cells[wI].posX;
+      const y = cells[wI].posY;
+      cells.map(cell => {
+        if (cell.isNeghbor(x, y)) {
+          cell.isWarning = true;
         }
+      });
+      wumpus = { x, y, i: wI };
     }
+  }
 
-    // place player
-    while (!player) {
-        let pI = Math.floor(Math.random() * cells.length);
-        if (!cells[pI].isPit && !cells[pI].isWarning && !cells[pI].isWumpus) {
-            cells[pI].isPlayer = true;
-            cells[pI].visit();
-            player = { x: cells[pI].posX, y: cells[pI].posY, i: pI };
-        }
+  // place player
+  while (!player) {
+    let pI = Math.floor(Math.random() * cells.length);
+    if (!cells[pI].isPit && !cells[pI].isWarning && !cells[pI].isWumpus) {
+      cells[pI].isPlayer = true;
+      cells[pI].visit();
+      player = { x: cells[pI].posX, y: cells[pI].posY, i: pI };
     }
-    gameOver = false;
-    return { cells, trapLocations, player, wumpus };
+  }
+  gameOver = false;
+  return { cells, trapLocations, player, wumpus };
 }
 
 function findOut(cell, showAlert) {
-    if (cell.isPit) {
-        gameOver = true;
-        drawGameboard();
-        alert("You fell in a pit and died!");
-    } else if (cell.isWumpus) {
-        gameOver = true;
-        drawGameboard();
-        alert("You stumble upon the Wumpus. It eats you. You died!");
-    } else if (cell.isWarning && showAlert) {
-        alert("You can hear breathing neaerby, the Wumpus is close!");
-    }
+  if (cell.isPit) {
+    gameOver = true;
+    drawGameboard();
+    alert("You fell in a pit and died!");
+  } else if (cell.isWumpus) {
+    gameOver = true;
+    drawGameboard();
+    alert("You stumble upon the Wumpus. It eats you. You died!");
+  } else if (cell.isWarning && showAlert) {
+    alert("You can hear breathing neaerby, the Wumpus is close!");
+  }
 
 }
 
 function updatePlayerLocation(newCell, oldCell) {
-    newCell.isPlayer = true;
-    oldCell.isPlayer = false;
-    player.x = newCell.posX;
-    player.y = newCell.posY;
-    player.i = cells.indexOf(newCell);
-    cells[player.i].visit();
-    drawGameboard();
+  newCell.isPlayer = true;
+  oldCell.isPlayer = false;
+  player.x = newCell.posX;
+  player.y = newCell.posY;
+  player.i = cells.indexOf(newCell);
+  cells[player.i].visit();
+  drawGameboard();
 }
 
 function movePlayer(key) {
-    const prevCell = cells[player.i]
-    switch (key) {
-        case "ArrowUp":
-            console.log('U');
-            if (prevCell.exitTop) {
-                const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y - 1)[0];
-                const showAlert = newCell.suppressAlert;
-                updatePlayerLocation(newCell, prevCell);
-                findOut(newCell, showAlert);
-            } else {
-                console.log("Bonk!");
-            }
-            break;
-        case "ArrowRight":
-            console.log('R');
-            if (prevCell.exitRight) {
-                const newCell = cells.filter((cell) => cell.posX === player.x + 1 && cell.posY === player.y)[0];
-                const showAlert = newCell.suppressAlert;
-                updatePlayerLocation(newCell, prevCell);
-                findOut(newCell);
-            } else {
-                console.log("Bonk!");
-            }
-            break;
-        case "ArrowDown":
-            console.log('D');
-            if (prevCell.exitDown) {
-                const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y + 1)[0];
-                const showAlert = newCell.suppressAlert;
-                updatePlayerLocation(newCell, prevCell);
-                findOut(newCell);
-            } else {
-                console.log("Bonk!");
-            }
-            break;
-        case "ArrowLeft":
-            console.log('L');
-            if (prevCell.exitLeft) {
-                const newCell = cells.filter((cell) => cell.posX === player.x - 1 && cell.posY === player.y)[0];
-                const showAlert = newCell.suppressAlert;
-                updatePlayerLocation(newCell, prevCell);
-                findOut(newCell);
-            } else {
-                console.log("Bonk!");
-            }
-            break;
-    }
+  const prevCell = cells[player.i]
+  switch (key) {
+    case "ArrowUp":
+      console.log('U');
+      if (prevCell.exitTop) {
+        const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y - 1)[0];
+        const showAlert = newCell.suppressAlert;
+        updatePlayerLocation(newCell, prevCell);
+        findOut(newCell, showAlert);
+      } else {
+        console.log("Bonk!");
+      }
+      break;
+    case "ArrowRight":
+      console.log('R');
+      if (prevCell.exitRight) {
+        const newCell = cells.filter((cell) => cell.posX === player.x + 1 && cell.posY === player.y)[0];
+        const showAlert = newCell.suppressAlert;
+        updatePlayerLocation(newCell, prevCell);
+        findOut(newCell);
+      } else {
+        console.log("Bonk!");
+      }
+      break;
+    case "ArrowDown":
+      console.log('D');
+      if (prevCell.exitDown) {
+        const newCell = cells.filter((cell) => cell.posX === player.x && cell.posY === player.y + 1)[0];
+        const showAlert = newCell.suppressAlert;
+        updatePlayerLocation(newCell, prevCell);
+        findOut(newCell);
+      } else {
+        console.log("Bonk!");
+      }
+      break;
+    case "ArrowLeft":
+      console.log('L');
+      if (prevCell.exitLeft) {
+        const newCell = cells.filter((cell) => cell.posX === player.x - 1 && cell.posY === player.y)[0];
+        const showAlert = newCell.suppressAlert;
+        updatePlayerLocation(newCell, prevCell);
+        findOut(newCell);
+      } else {
+        console.log("Bonk!");
+      }
+      break;
+  }
 };
 
 function fireArrow(key) {
-    switch (key) {
-        case "ArrowUp":
-            console.log('Arrow U');
-            if (cells[player.i].exitTop) {
-                if (player.y - 1 === wumpus.y && player.x === wumpus.x) {
-                    cells[wumpus.i].visit();
-                    drawGameboard();
-                    alert("You killed the Wumpus. Good Job!")
-                    win = true;
-                } else {
-                    alert("You missed! The Wumpus jumps out and eats you. You died!");
-                }
-                gameOver = true;
-                drawGameboard();
-            }
-            break;
-        case "ArrowRight":
-            console.log('Arrow R');
-            if (cells[player.i].exitRight) {
-                if (player.x + 1 === wumpus.x && player.y === wumpus.y) {
-                    cells[wumpus.i].visit();
-                    drawGameboard();
-                    alert("You killed the Wumpus. Good Job!")
-                    win = true;
-                } else {
-                    alert("You missed! The Wumpus jumps out and eats you. You died!");
-                }
-                gameOver = true;
-                drawGameboard();
-            }
-            break;
-        case "ArrowDown":
-            console.log('Arrow D');
-            if (cells[player.i].exitDown) {
-                if (player.y + 1 === wumpus.y && player.x === wumpus.x) {
-                    cells[wumpus.i].visit();
-                    drawGameboard();
-                    alert("You killed the Wumpus. Good Job!")
-                    win = true;
-                } else {
-                    alert("You missed! The Wumpus jumps out and eats you. You died!");
-                }
-                gameOver = true;
-                drawGameboard();
-            }
-            break;
-        case "ArrowLeft":
-            console.log('Arrow L');
-            if (cells[player.i].exitLeft) {
-                if (player.x - 1 === wumpus.x && player.y === wumpus.y) {
-                    cells[wumpus.i].visit();
-                    drawGameboard();
-                    alert("You killed the Wumpus. Good Job!")
-                    win = true;
-                } else {
-                    alert("You missed! The Wumpus jumps out and eats you. You died!");
-                }
-                gameOver = true;
-                drawGameboard();
-            }
-            break;
-    }
+  switch (key) {
+    case "ArrowUp":
+      console.log('Arrow U');
+      if (cells[player.i].exitTop) {
+        if (player.y - 1 === wumpus.y && player.x === wumpus.x) {
+          cells[wumpus.i].visit();
+          drawGameboard();
+          alert("You killed the Wumpus. Good Job!")
+          win = true;
+        } else {
+          alert("You missed! The Wumpus jumps out and eats you. You died!");
+        }
+        gameOver = true;
+        drawGameboard();
+      }
+      break;
+    case "ArrowRight":
+      console.log('Arrow R');
+      if (cells[player.i].exitRight) {
+        if (player.x + 1 === wumpus.x && player.y === wumpus.y) {
+          cells[wumpus.i].visit();
+          drawGameboard();
+          alert("You killed the Wumpus. Good Job!")
+          win = true;
+        } else {
+          alert("You missed! The Wumpus jumps out and eats you. You died!");
+        }
+        gameOver = true;
+        drawGameboard();
+      }
+      break;
+    case "ArrowDown":
+      console.log('Arrow D');
+      if (cells[player.i].exitDown) {
+        if (player.y + 1 === wumpus.y && player.x === wumpus.x) {
+          cells[wumpus.i].visit();
+          drawGameboard();
+          alert("You killed the Wumpus. Good Job!")
+          win = true;
+        } else {
+          alert("You missed! The Wumpus jumps out and eats you. You died!");
+        }
+        gameOver = true;
+        drawGameboard();
+      }
+      break;
+    case "ArrowLeft":
+      console.log('Arrow L');
+      if (cells[player.i].exitLeft) {
+        if (player.x - 1 === wumpus.x && player.y === wumpus.y) {
+          cells[wumpus.i].visit();
+          drawGameboard();
+          alert("You killed the Wumpus. Good Job!")
+          win = true;
+        } else {
+          alert("You missed! The Wumpus jumps out and eats you. You died!");
+        }
+        gameOver = true;
+        drawGameboard();
+      }
+      break;
+  }
 };
 
 
 
 // draw gameboard
 function drawGameboard () {
-    const gameBoard = document.getElementById("game-board");
-    gameBoard.innerHTML = "";
-    cells.forEach(cell => {
-        const cellDiv = document.createElement('div')
-        cellDiv.classList.add('cell');
-        if (cell.isPlayer) {
-            arrowMode ? cellDiv.classList.add('aiming') : cellDiv.classList.add('player');
-        }
-        if (cell.visited || gameOver) {
-            cellDiv.classList.remove('fog');
-            if (cell.exitTop) {
-                cellDiv.classList.add('top-door');
-            }
-            if (cell.exitRight) {
-                cellDiv.classList.add('right-door');
-            }
-            if (cell.exitDown) {
-                cellDiv.classList.add('bottom-door');
-            }
-            if (cell.exitLeft) {
-                cellDiv.classList.add('left-door');
-            }
-            if (cell.isPit) {
-                cellDiv.classList.add('trap');
-            }
-            if (cell.isWumpus) {
-                cellDiv.classList.add('wumpus');
-            }
-            if (cell.isWarning && !cell.isWumpus) {
-                cellDiv.classList.add('warning');
-            }
-        } else {
-            cellDiv.classList.add('fog');
-        }
-        if (gameOver) {
-            cellDiv.classList.remove('fog');
-            if (cell.isPlayer) {
-                const deadPlayer = document.createElement('div')
-                deadPlayer.classList.add('dead')
-                deadPlayer.classList.add('player');
-                cellDiv.appendChild(deadPlayer)
-            }
-        }
-        cellDiv.style = `grid-column-start: ${cell.posX + 1}; grid-column-end: ${cell.posX + 2}; grid-row-start: ${cell.posY + 1}; grid-row-end: ${cell.posY + 2};`
-        gameBoard.appendChild(cellDiv)
-    })
+  const gameBoard = document.getElementById("game-board");
+  gameBoard.innerHTML = "";
+  cells.forEach(cell => {
+    const cellDiv = document.createElement('div')
+    cellDiv.classList.add('cell');
+    if (cell.isPlayer) {
+      arrowMode ? cellDiv.classList.add('aiming') : cellDiv.classList.add('player');
+    }
+    if (cell.visited || gameOver) {
+      cellDiv.classList.remove('fog');
+      if (cell.exitTop) {
+        cellDiv.classList.add('top-door');
+      }
+      if (cell.exitRight) {
+        cellDiv.classList.add('right-door');
+      }
+      if (cell.exitDown) {
+        cellDiv.classList.add('bottom-door');
+      }
+      if (cell.exitLeft) {
+        cellDiv.classList.add('left-door');
+      }
+      if (cell.isPit) {
+        cellDiv.classList.add('trap');
+      }
+      if (cell.isWumpus) {
+        cellDiv.classList.add('wumpus');
+      }
+      if (cell.isWarning && !cell.isWumpus) {
+        cellDiv.classList.add('warning');
+      }
+    } else {
+      cellDiv.classList.add('fog');
+    }
+    if (gameOver) {
+      cellDiv.classList.remove('fog');
+      if (cell.isPlayer) {
+        const deadPlayer = document.createElement('div')
+        deadPlayer.classList.add('dead')
+        deadPlayer.classList.add('player');
+        cellDiv.appendChild(deadPlayer)
+      }
+    }
+    cellDiv.style = `grid-column-start: ${cell.posX + 1}; grid-column-end: ${cell.posX + 2}; grid-row-start: ${cell.posY + 1}; grid-row-end: ${cell.posY + 2};`
+    gameBoard.appendChild(cellDiv)
+  })
 };
 
 
@@ -249,22 +249,22 @@ drawGameboard();
 
 // event handlers
 const handleNewGameClick = (e) => {
-    location.reload();
+  location.reload();
 }
 
 const handleKeyPress = ({ key }) => {
-    if (gameOver) return;
-    if (key === 'a') {
-        arrowMode = !arrowMode;
-        drawGameboard();
+  if (gameOver) return;
+  if (key === 'a') {
+    arrowMode = !arrowMode;
+    drawGameboard();
+  }
+  if (key === "ArrowUp" || key === "ArrowRight" || key === "ArrowDown" || key === "ArrowLeft") {
+    if (arrowMode) {
+      fireArrow(key)
+    } else {
+      movePlayer(key)
     }
-    if (key === "ArrowUp" || key === "ArrowRight" || key === "ArrowDown" || key === "ArrowLeft") {
-        if (arrowMode) {
-            fireArrow(key)
-        } else {
-            movePlayer(key)
-        }
-    }
+  }
 }
 
 
